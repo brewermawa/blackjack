21a02b59bec02b70a4eb7583d74e278b7292d279
Mawa Guerra <brewermawa@gmail.com>
2026-01-18 12:23:42 -0600

    BlackJackEval done

--- blackjack_eval.py
+++ blackjack_eval.py
@@ -1,2 +1,120 @@
+from cards.hand import Hand
+
 class BlackJackEval:
-    pass
+    """
+    - blackjack(cls, hand: Hand) -> bool
+    - can_double(cls, hand: Hand, valid_doubles: list=[]) -> bool
+    - can_split(cls, hand: Hand) -> bool
+    - soft(cls, hand: Hand) -> bool
+    - value(cls, hand: Hand) -> int
+    - bust(cls, hand: Hand) -> bool
+    """
+
+    @classmethod
+    def _validate_is_hand(cls, hand: Hand) -> None:
+        if not isinstance(hand, Hand):
+            raise ValueError("hand must be an instance of the Hand class")
+    
+    @classmethod
+    def _validate_two_cards(cls, hand: Hand) -> None:
+        if len(hand) != 2:
+            raise ValueError("hand must contain exactly 2 cards")
+        
+    @classmethod
+    def _validate_two_or_more_cards(cls, hand: Hand) -> None:
+        if len(hand) < 2:
+            raise ValueError("hand must contain 2 or more cards")
+
+    @classmethod
+    def blackjack(cls, hand: Hand) -> bool:
+        cls._validate_is_hand(hand)
+        cls._validate_two_cards(hand)
+        
+        #Check for blackjack
+        ace_in_hand = any(card.rank == "A" for card in hand.cards)
+        ten_in_hand = any(card.rank in ["10", "J", "Q", "K"] for card in hand.cards)
+
+        return ace_in_hand and ten_in_hand
+
+
+    @classmethod
+    def can_double(cls, hand: Hand, valid_doubles: list = None) -> bool:
+        cls._validate_is_hand(hand)
+        cls._validate_two_cards(hand)
+
+        if valid_doubles is None:
+            valid_doubles = []
+       
+        #validate valid_doubles is a list
+        if not isinstance(valid_doubles, list):
+            raise ValueError("valid_doubles must be a list")
+        
+        #validate valid_doubles members are all ints
+        for val in valid_doubles:
+            if not isinstance(val, int) or isinstance(val, bool):
+                raise ValueError("all valid_doubles members must be integers")
+            
+        if len(valid_doubles) ==  0:
+            return True
+        
+        if cls.value(hand) in valid_doubles:
+            return True
+        
+        return False
+
+
+    @classmethod
+    def can_split(cls, hand: Hand) -> bool:
+        cls._validate_is_hand(hand)
+        cls._validate_two_cards(hand)
+        
+        if hand.cards[0].rank == hand.cards[1].rank:
+            return True
+        
+        if hand.cards[0].rank in ["10", "J", "Q", "K"] and hand.cards[1].rank in ["10", "J", "Q", "K"]:
+            return True
+        
+        return False
+
+
+    @classmethod
+    def soft(cls, hand: Hand) -> bool:
+        cls._validate_is_hand(hand)
+        cls._validate_two_or_more_cards(hand)
+        
+        ace_in_hand = any(card.rank == "A" for card in hand.cards)
+
+        if ace_in_hand and cls.value(hand) <= 21:
+            return True
+        
+        return False
+
+    @classmethod
+    def value(cls, hand: Hand) -> int:
+        cls._validate_is_hand(hand)
+        cls._validate_two_or_more_cards(hand)
+        
+        #Replace J, Q and K with 10
+        ranks_JQK_as_10 = ["10" if card.rank in ["J", "Q", "K"] else card.rank for card in hand.cards]
+        ace_in_hand = any(rank == "A" for rank in ranks_JQK_as_10)
+
+        if not ace_in_hand:
+            ranks = [int(rank) for rank in ranks_JQK_as_10]
+            return sum(ranks)
+        
+        #Ace in hand
+        #Cast ranks as int, A's converted to 1
+        ranks_int = [1 if rank == "A" else int(rank) for rank in ranks_JQK_as_10]
+
+        if sum(ranks_int) + 10 <= 21:
+            return sum(ranks_int) + 10
+        else:
+            return sum(ranks_int)
+
+
+    @classmethod
+    def bust(cls, hand: Hand) -> bool:
+        cls._validate_is_hand(hand)
+        cls._validate_two_or_more_cards(hand)
+
+        return cls.value(hand) > 21
--- tests/test_blackjackeval.py
+++ tests/test_blackjackeval.py
@@ -13,6 +13,7 @@ from cards.hand import Hand
 - bust(cls, hand: Hand) -> bool
 """
 
+
 class TestBlackjackEvalTypeValidation:
     @pytest.mark.parametrize(
         "method_to_test",
@@ -53,6 +54,7 @@ class TestBlackjackEvalTypeValidation:
     def test_all_methods_raise_value_error_if_hand_not_hand_instance(self, method_to_test, invalid_hand):
         with pytest.raises(ValueError):
             method_to_test(invalid_hand)
+
 
 class TestBlackjackEval:
     hand_zero_cards = Hand()
@@ -113,6 +115,45 @@ class TestBlackjackEval:
     hand_k10.add_card(Card("K", "♣"))
     hand_k10.add_card(Card("10", "♦"))
 
+    hand_a82 = Hand()
+    hand_a82.add_card(Card("A", "♣"))
+    hand_a82.add_card(Card("8", "♦"))
+    hand_a82.add_card(Card("2", "♦"))
+    
+    hand_aa8 = Hand()
+    hand_aa8.add_card(Card("A", "♣"))
+    hand_aa8.add_card(Card("A", "♦"))
+    hand_aa8.add_card(Card("8", "♣"))
+
+    hand_aaa6 = Hand()
+    hand_aaa6.add_card(Card("A", "♣"))
+    hand_aaa6.add_card(Card("A", "♦"))
+    hand_aaa6.add_card(Card("A", "♥"))
+    hand_aaa6.add_card(Card("6", "♣"))
+
+    hand_aaa9 = Hand()
+    hand_aaa9.add_card(Card("A", "♣"))
+    hand_aaa9.add_card(Card("A", "♦"))
+    hand_aaa9.add_card(Card("A", "♥"))
+    hand_aaa9.add_card(Card("9", "♣"))
+
+    hand_bust_996 = Hand()
+    hand_bust_996.add_card(Card("9", "♣"))
+    hand_bust_996.add_card(Card("9", "♦"))
+    hand_bust_996.add_card(Card("6", "♥"))
+    
+    hand_bust_93J = Hand()
+    hand_bust_93J.add_card(Card("9", "♣"))
+    hand_bust_93J.add_card(Card("3", "♦"))
+    hand_bust_93J.add_card(Card("J", "♥"))
+
+    hand_bust_AA46J = Hand()
+    hand_bust_AA46J.add_card(Card("A", "♥"))
+    hand_bust_AA46J.add_card(Card("A", "♦"))
+    hand_bust_AA46J.add_card(Card("4", "♥"))
+    hand_bust_AA46J.add_card(Card("6", "♥"))
+    hand_bust_AA46J.add_card(Card("J", "♥"))
+
     @pytest.mark.parametrize(
         "hand",
         [hand_zero_cards, hand_one_card, hand_three_cards]
@@ -120,7 +161,7 @@ class TestBlackjackEval:
     def test_blackjack_raises_valueerror_if_hand_does_not_contain_exactly_two_cards(self, hand):
         with pytest.raises(ValueError):
             BlackJackEval.blackjack(hand)
-    
+
     @pytest.mark.parametrize(
         "hand",
         [hand_ak, hand_aq, hand_aj, hand_a10]
@@ -145,7 +186,7 @@ class TestBlackjackEval:
 
     @pytest.mark.parametrize(
         "valid_doubles_not_list",
-        [(9, 10, 11), {9, 10, 11}, 11, "11", None]
+        [(9, 10, 11), {9, 10, 11}, 11, "11"]
     )
     def test_can_double_raises_valueerror_if_valid_doubles_is_not_list(self, valid_doubles_not_list):
         with pytest.raises(ValueError):
@@ -161,6 +202,9 @@ class TestBlackjackEval:
 
     def test_can_double_returns_true_with_valid_doubles_list_empty(self):
         assert BlackJackEval.can_double(self.hand_87) is True
+
+    def test_can_double_returns_true_with_valid_doubles_list_set_and_hand_value_in_list(self):
+        assert BlackJackEval.can_double(self.hand_83, [9, 10, 11]) is True
 
     def test_can_double_returns_false_with_valid_doubles_list_set_and_hand_value_not_in_list(self):
         assert BlackJackEval.can_double(self.hand_62, [9, 10, 11]) is False
@@ -186,3 +230,83 @@ class TestBlackjackEval:
     )
     def test_can_split_returns_false_if_the_ranks_in_hand_are_not_pair(self, hand_not_pair):
         assert BlackJackEval.can_split(hand_not_pair) is False
+
+    @pytest.mark.parametrize(
+        "hand",
+        [hand_zero_cards, hand_one_card]
+    )
+    def test_soft_raises_valueerror_if_hand_contains_less_than_two_cards(self, hand):
+        with pytest.raises(ValueError):
+            BlackJackEval.soft(hand)
+
+    @pytest.mark.parametrize(
+        "hand_is_soft",
+        [hand_a2, hand_a10, hand_a82, hand_aa8, hand_aaa6]
+    )
+    def test_soft_returns_true_if_the_hand_is_soft(self, hand_is_soft):
+        assert BlackJackEval.soft(hand_is_soft) is True
+
+    @pytest.mark.parametrize(
+        "hand_not_soft",
+        [hand_62, hand_66, hand_kj]
+    )
+    def test_soft_returns_false_if_the_hand_is_not_soft(self, hand_not_soft):
+        assert BlackJackEval.soft(hand_not_soft) is False
+
+    @pytest.mark.parametrize(
+        "hand",
+        [hand_zero_cards, hand_one_card]
+    )
+    def test_value_raises_valueerror_if_hand_contains_less_than_two_cards(self, hand):
+        with pytest.raises(ValueError):
+            BlackJackEval.value(hand)
+
+    @pytest.mark.parametrize(
+        "hand, result",
+        [
+            (hand_k10, 20),
+            (hand_a2, 13),
+            (hand_a82, 21),
+            (hand_aaa6, 19),
+            (hand_aaa9, 12),
+            (hand_bust_996, 24),
+            (hand_bust_93J, 22),
+            (hand_bust_AA46J, 22)
+        ]
+    )
+    def test_value_returns_correct_value(self, hand, result):
+        assert BlackJackEval.value(hand) == result
+
+    @pytest.mark.parametrize(
+        "hand",
+        [hand_zero_cards, hand_one_card]
+    )
+    def test_bust_raises_valueerror_if_hand_contains_less_than_two_cards(self, hand):
+        with pytest.raises(ValueError):
+            BlackJackEval.bust(hand)
+
+    @pytest.mark.parametrize(
+        "hand_bust",
+        [
+            hand_bust_996,
+            hand_bust_93J,
+            hand_bust_AA46J
+        ]
+    )
+    def test_bust_returns_true_when_hand_value_over_21(self, hand_bust):
+        assert BlackJackEval.bust(hand_bust) is True
+
+    @pytest.mark.parametrize(
+        "hand",
+        [
+            hand_k10,
+            hand_a2,
+            hand_a82,
+            hand_aaa6,
+            hand_aaa9
+        ]
+    )
+    def test_bust_returns_false_when_hand_value_less_than_or_equal_to_21(self, hand):
+        assert BlackJackEval.bust(hand) is False
+
+
